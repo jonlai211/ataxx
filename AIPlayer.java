8929dc10285d577c32c7ba4d755a00e9d79ab05a
whoismz <whoismz@outlook.com>
2023-05-18 20:05:48 +0800

    update: details

--- AIPlayer.java
+++ AIPlayer.java
@@ -47,20 +47,10 @@ class AIPlayer extends Player {
         // Therefore, the following codes should be modified
         // in order to meet with the requirements of Part A.2.
         // You can create add your own method and put your method here.
-		
-//        ArrayList<Move> listOfMoves = possibleMoves(b, b.nextMove());
-//        int moveArrayLength = listOfMoves.size();
-//        int randomIndex = (int) (Math.random() * moveArrayLength);
-//        for(int i = 0; i < moveArrayLength; i++){
-//            if (i == randomIndex){
-//                b.createMove(listOfMoves.get(i));
-//                lastFoundMove = listOfMoves.get(i);
-//            }
-//        }
 
         if (b.nextMove() == PieceState.RED) {
             findMove(b, MAX_DEPTH, true, 1, -INFTY, INFTY);
-        } else {
+        } else if (b.nextMove() == PieceState.BLUE) {
             findMove(b, MAX_DEPTH, true, -1, -INFTY, INFTY);
         }
 
